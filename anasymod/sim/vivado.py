ce225f1e1661753461a4587a97fa29d8efdd21fa
nsulzer <niels.sulzer@web.de>
2022-08-01 14:12:14 +0200

    use custom TCL scripts in simulation

--- anasymod/sim/vivado.py
+++ anasymod/sim/vivado.py
@@ -1,6 +1,7 @@
 import re
 from anasymod.sim.sim import Simulator
 from anasymod.generators.vivado import VivadoTCLGenerator
+from anasymod.util import back2fwd
 
 class VivadoSimulator(Simulator):
     def simulate(self):
@@ -27,6 +28,20 @@ class VivadoSimulator(Simulator):
         # if desired, treat Verilog (*.v) files as SystemVerilog (*.sv)
         if self.target.prj_cfg.cfg.treat_v_as_sv:
             v.writeln('set_property file_type SystemVerilog [get_files -filter {FILE_TYPE == Verilog}]')
+
+        # read user-provided TCL scripts
+        v.writeln('# Custom user-provided TCL scripts')
+        for tcl_file in v.target.content.tcl_files:
+            for file in tcl_file.files:
+                v.writeln(f'source "{back2fwd(file)}"')
+
+        # upgrade IPs as necessary
+        v.writeln('if {[get_ips] ne ""} {')
+        v.writeln('    upgrade_ip [get_ips]')
+        v.writeln('}')
+
+        # generate all IPs
+        v.writeln('generate_target all [get_ips]')
 
         # launch the simulation
         v.set_property('{xsim.simulate.runtime}', '{-all}', '[get_fileset sim_1]')
